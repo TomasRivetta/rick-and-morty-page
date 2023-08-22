dc7ffec9d83f00d40af024ee73c02a499a0e59c3
TomasRivetta <404954@tecnicatura.frc.utn.edu.ar>
2023-08-22 17:38:06 -0300

    eliminando seccion de barra de busqueda

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const loader = document.querySelector('.lds-facebook')
 const next = document.querySelector('#next')
 const prev = document.querySelector('#prev')
 
-const btnSearch = document.querySelector(".btnSearch")
-
 let contador = 1;
 
 const listCharacters = async (page = 1) => {
@@ -54,67 +52,6 @@ window.addEventListener('hashchange', () => {
     localStorage.setItem('charId', id)
     //la siguiente linea me manda al pages de caracter
     window.location.replace('/pages/character.html')
-})
-
-//FILTRAR LOS PERSONAJES POR NOMBRE
-const mostrar = async (name) => {
-
-    contador = 1;
-
-    container.innerHTML = "";
-
-    loader.style.display = "inline-block";
-
-    try {
-        const { results, info } = await getCharacterForFilter(name)
-
-        console.log(info);
-
-        console.log(info.next);
-        console.log(info.prev);
-
-        if (info.count > 0) {
-
-            loader.style.display = "none";
-
-            results.forEach(character => {
-
-                const article = document.createElement('article')
-                article.setAttribute('class', 'container-tarjeta')
-
-                article.innerHTML = `
-                <div class="card" style="width: 18rem;">
-                    <img src="${character.image}" class="card-img-top" alt="imagen de ${character.name}">
-                    <div class="card-body">
-                        <h5 class="card-title">${character.name}</h5>
-                        <p class="">${character.status} - ${character.species}</p>
-                        <p class="">${character.origin.name}</p>
-                    <a href="/#/${character.id}" class="btn-detail btn btn-primary">Ver detalle</a>
-                    </div>
-                </div>`
-                container.appendChild(article)
-
-            });
-        }
-
-    } catch (error) {
-        console.log(error);
-        swal({
-            icon: "error",
-            title: "No existe el personaje Buscado!",
-        });
-        next.style.display = "none";
-        prev.style.display = "none";
-    }
-
-}
-
-btnSearch.addEventListener("click", () => {
-    const valorSearchInput = document.querySelector(".txtSearch").value
-    mostrar(valorSearchInput);
-    document.querySelector(".txtSearch").value = "";
-    next.style.display = "inline-block";
-    prev.style.display = "inline-block";
 })
 
 //PAGINACION
